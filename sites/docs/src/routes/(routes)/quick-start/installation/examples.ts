94e7204cad1009bab64e8ad692a40cd877eb203b
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-21 17:42:26 +0100

    fix: styling

--- sites/docs/src/routes/(routes)/quick-start/installation/examples.ts
+++ sites/docs/src/routes/(routes)/quick-start/installation/examples.ts
@@ -2,10 +2,20 @@
 const codeBlock = `✔ Where do you want to place components ./src/lib/components/tzezars-enhancements
 ✔ Select components to install (use space to select multiple) avatar-group`
 
+const usageCode = `<script lang="ts">
+	import { CodeSnippet } from '$lib/components/tzezars-enhancements/code-snippet';
+</script>
+
+<CodeSnippet content="" />`
+
+
 
 const examples = {
     promt: {
         code: codeBlock
+    },
+    usage: {
+        code: usageCode
     }
 }
 
