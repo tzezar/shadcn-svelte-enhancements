cf2cc6e1850fc114e3eb0e4382df584bb2128309
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-21 14:47:48 +0100

    feat: added shadcn-svelte

---
+++ src/lib/components/ui/sidebar/sidebar-inset.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import type { WithElementRef } from "bits-ui";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLElement>> = $props();
+</script>
+
+<main
+	bind:this={ref}
+	class={cn(
+		"bg-background flex flex-col w-full",
+		// md:peer-data-[variant=inset]:m-2
+		"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))]   md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:shadow",
+		className
+	)}
+	{...restProps}
+>
+	{@render children?.()}
+</main>
