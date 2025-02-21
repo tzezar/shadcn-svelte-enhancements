3cee246f696f0cd640086b0064575418620db2f2
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-21 19:23:28 +0100

    docs: improved installation guide

--- sites/docs/src/routes/(routes)/quick-start/installation/examples.ts
+++ sites/docs/src/routes/(routes)/quick-start/installation/examples.ts
@@ -1,6 +1,10 @@
 
-const codeBlock = `✔ Where do you want to place components ./src/lib/components/tzezars-enhancements
-✔ Select components to install (use space to select multiple) avatar-group`
+const codeBlock = `✔ Where do you want to place components? ./src/lib/components/tzezars-enhancements
+✔ Select components to install (use space to select multiple):
+[x] avatar-group
+[x] code-block
+[ ] code-snippet
+... `
 
 const usageCode = `<script lang="ts">
 	import { CodeSnippet } from '$lib/components/tzezars-enhancements/code-snippet';
