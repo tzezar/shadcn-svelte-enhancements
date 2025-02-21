26c4cf17c6c2cb80c9653b165e1e1c38e8a0cc17
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-21 17:18:28 +0100

    fix: layout bug

--- sites/docs/src/lib/components/ui/sidebar/sidebar-inset.svelte
+++ sites/docs/src/lib/components/ui/sidebar/sidebar-inset.svelte
@@ -11,10 +11,11 @@
 	}: WithElementRef<HTMLAttributes<HTMLElement>> = $props();
 </script>
 
+
 <main
 	bind:this={ref}
 	class={cn(
-		"bg-background flex flex-col w-full",
+		"bg-background flex flex-col w-full overflow-hidden",
 		// md:peer-data-[variant=inset]:m-2
 		"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))]   md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:shadow",
 		className
