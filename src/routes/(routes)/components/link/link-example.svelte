3e11686665329ce83741c5196dad9e11d3dbc606
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-21 14:48:45 +0100

    docs: link

---
+++ src/routes/(routes)/components/link/link-example.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Link } from '$lib/components/tzezars-enhancements/link';
+</script>
+
+<div class="flex flex-col gap-4 px-8">
+	<p>Check out these amazing Middle-earth experiences:</p>
+
+	<div>
+		<Link href="https://www.mcmiddleearth.com/" target="_blank" class="!text-foreground">
+			Minecraft Middle Earth
+		</Link>
+		<p class='text-sm'>
+			Dive into the breathtaking world of Middle-earth recreated block by block in Minecraft, where
+			you can explore iconic locations from The Lord of the Rings and The Hobbit.
+		</p>
+	</div>
+
+	<div>
+		<Link
+			href="https://www.youtube.com/watch?v=17h5tlYSsS0"
+			target="_blank"
+			class="!text-foreground"
+		>
+			The Lord of the Rings trilogy but it's just the memes
+		</Link>
+		<p class='text-sm'>
+			Watch iconic scenes from The Lord of the Rings trilogy that became the foundation for many of
+			the most popular memes on the internet.
+		</p>
+	</div>
+</div>
---
+++ src/routes/(routes)/components/link/link-usage.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { Link } from '$lib/components/tzezars-enhancements/link';
+</script>
+
+<Link href="Hello There">General Kenobi</Link>
