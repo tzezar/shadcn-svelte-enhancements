81927869ff4fb7bd750ee629ee9e633568ad931b
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2025-02-22 03:15:19 +0100

    fix:

--- sites/docs/src/routes/(routes)/+layout.svelte
+++ sites/docs/src/routes/(routes)/+layout.svelte
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 	import '../../app.css';
-	import { ModeWatcher } from "mode-watcher";
+	import { ModeWatcher } from 'mode-watcher';
 
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
@@ -16,19 +16,19 @@
 	import { page } from '$app/stores';
 	import { SIDEBAR_WIDTH } from '$lib/components/ui/sidebar/constants';
 
-
 	let { children } = $props();
 
-
-	let pathSegments = $derived($page.url.pathname
-    .split('/')
-    .filter(Boolean)
-    .map((segment, index, array) => ({
-      label: segment.charAt(0).toUpperCase() + segment.slice(1),
-      href: '/' + array.slice(0, index + 1).join('/')
-    })))
-
+	let pathSegments = $derived(
+		$page.url.pathname
+			.split('/')
+			.filter(Boolean)
+			.map((segment, index, array) => ({
+				label: segment.charAt(0).toUpperCase() + segment.slice(1),
+				href: '/' + array.slice(0, index + 1).join('/')
+			}))
+	);
 </script>
+
 <ModeWatcher />
 <Sidebar.Provider>
 	<AppSidebar />
@@ -37,17 +37,17 @@
 	</div> -->
 
 	<Sidebar.Inset>
-		<header class="flex h-16 shrink-0 items-center gap-2 sticky top-0 z-50 bg-background border-b">
-			<div class="flex items-center gap-2 px-4 z-[100]">
+		<header class="bg-background sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 border-b">
+			<div class="z-[100] flex items-center gap-2 px-4">
 				<Sidebar.Trigger class="-ml-1" />
 				<Separator orientation="vertical" class="mr-2 h-4" />
 				<Breadcrumb.Root>
 					<Breadcrumb.List>
 						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href='/'>Home</Breadcrumb.Link>
+							<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
 						</Breadcrumb.Item>
 						{#each pathSegments as segment, i}
-						<Breadcrumb.Separator class="hidden md:block" />
+							<Breadcrumb.Separator class="hidden md:block" />
 							<Breadcrumb.Item class="hidden md:block">
 								<Breadcrumb.Link href={segment.href}>{segment.label}</Breadcrumb.Link>
 							</Breadcrumb.Item>
